f3b9274a0311005d3c7440aaea1802c2ad2c1907
Thomas Braccia <bracciata@gmail.com>
2019-02-08 22:25:24 -0600

    Added code from documentation
    For the twitter get tweets added documentation code

--- HackKU2019/GetTweets.cs
+++ HackKU2019/GetTweets.cs
@@ -1,9 +1,34 @@
+using System;
 using System.Net;
+using System.Net.Http;
 
 namespace HackKU2019
 {
     public class GetTweets
     {
-        HttpWebRequest twitterRequest= (HttpWebRequest)WebRequest.Create("http://apiURLForLaterKU.com");
+        public async void pullTweets()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //The following code is borrowed from Microsoft documentation at https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=netframework-4.7.2
+                // Call asynchronous network methods in a try/catch block to handle exceptions
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Above three lines can be replaced with new helper method below
+                    // string responseBody = await client.GetStringAsync(uri);
+
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+        }
     }
 }
