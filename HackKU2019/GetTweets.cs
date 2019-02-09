94e346a6f4f724f055e2ae81ece4dd2c4c1a6668
Thomas Braccia <bracciata@gmail.com>
2019-02-08 22:45:06 -0600

    Switched to tweetinvi
    Added a library for getting tweets

--- HackKU2019/GetTweets.cs
+++ HackKU2019/GetTweets.cs
@@ -1,34 +1,23 @@
 using System;
 using System.Net;
 using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 namespace HackKU2019
 {
     public class GetTweets
     {
+        IAuthenticatedUser getUser()
+        {
+            var userCredentials = Auth.CreateCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            return User.GetAuthenticatedUser(userCredentials);
+        }
+        
         public async void pullTweets()
         {
-            using (HttpClient client = new HttpClient())
-            {
-                //The following code is borrowed from Microsoft documentation at https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=netframework-4.7.2
-                // Call asynchronous network methods in a try/catch block to handle exceptions
-                try
-                {
-                    HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
-                    response.EnsureSuccessStatusCode();
-
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    // Above three lines can be replaced with new helper method below
-                    // string responseBody = await client.GetStringAsync(uri);
-
-                    Console.WriteLine(responseBody);
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine("\nException Caught!");
-                    Console.WriteLine("Message :{0} ", e.Message);
-                }
-            }
+            var authUser = getUser();
         }
     }
 }
