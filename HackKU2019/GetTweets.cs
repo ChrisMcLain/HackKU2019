624cfcfbfc10ac10e35d1129b4bbaf6f93876395
Thomas Braccia <bracciata@gmail.com>
2019-02-08 23:24:43 -0600

    Updating twitter tweets pulling

--- HackKU2019/GetTweets.cs
+++ HackKU2019/GetTweets.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
+using Remotion.Linq.Clauses;
 using Tweetinvi;
 using Tweetinvi.Models;
 
@@ -9,14 +10,11 @@ namespace HackKU2019
 {
     public class GetTweets
     {
-
-        
         public void pullTweets(string handle)
         {
             Keys keys = new Keys();
             Auth.SetUserCredentials(keys.ConsumerKey, keys.ConsumerSecret, keys.TokenKey, keys.TokenSecret);
-            var tweets = Timeline.GetUserTimeline("bracciata");
-            Console.WriteLine(tweets);
+            var tweets = Timeline.GetUserTimeline(handle);
 
         }
     }
