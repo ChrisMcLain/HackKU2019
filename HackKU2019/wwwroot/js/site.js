ef429041de0489fdb15943f19cc80a254de2e93a
Thomas Braccia <bracciata@gmail.com>
2019-02-10 00:16:06 -0600

    More broken js

--- HackKU2019/wwwroot/js/site.js
+++ HackKU2019/wwwroot/js/site.js
@@ -4,10 +4,10 @@
 // Write your JavaScript code.
 window.onresize = function(event) {
     var issuesContainers= document.getElementsByClassName("issue_container");
-    console.log(issuesContainers.length);
     for (var container in issuesContainers){
-        var triangle = container.getElementsByClassName("arrow-left")[0];
-        var issues = container.getElementsByClassName("issue")[0];
+        console.log(container.style.height);
+        var triangle = container.firstChild;
+        var issues = triangle.nextSibling;
         var heightOfBoxDiv2 =issues.style.height/2;
         triangle.style.borderBottom=heightOfBoxDiv2+"px solid transparent;"
         triangle.style.borderTop=heightOfBoxDiv2+"px solid transparent;"
