cdca6da9de557f1aafc5823c1faf350e13a38d1e
Thomas Braccia <bracciata@gmail.com>
2019-02-09 23:59:22 -0600

    Broken temp js

--- HackKU2019/wwwroot/js/site.js
+++ HackKU2019/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.onresize = function(event) {
+    var issuesContainers= document.getElementsByClassName("issue_container");
+    console.log(issuesContainers.length);
+    for (var container in issuesContainers){
+        var triangle = container.getElementsByClassName("arrow-left")[0];
+        var issues = container.getElementsByClassName("issue")[0];
+        var heightOfBoxDiv2 =issues.style.height/2;
+        triangle.style.borderBottom=heightOfBoxDiv2+"px solid transparent;"
+        triangle.style.borderTop=heightOfBoxDiv2+"px solid transparent;"
+
+    }
+};
